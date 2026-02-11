f42712d2184d8716dbb2c8557711891736ba4836
Time <timetime2310@gmail.com>
2026-02-11 10:50:25 +0700

    add more bullet to used to pooling

--- Assets/src/1/collider test/ColliderTest.cs
+++ Assets/src/1/collider test/ColliderTest.cs
@@ -3,27 +3,45 @@ using UnityEngine;
 public class ColliderTest : MonoBehaviour
 {
     #region TesterScript
-    [SerializeField] Color testColor = Color.green;
+    [SerializeField] float colorChangeSpeed = 1f; // Control the rainbow speed
     private Color originalColor = Color.white;
     private Renderer rend;
+    private bool isRainbowActive = false; // Toggle for when the trigger is hit
+
     void Start()
     {
         rend = GetComponent<Renderer>();
-        // Store the starting color so we can reset to it
         if (rend != null) originalColor = rend.material.color;
     }
+
     void Update()
     {
+        // If the trigger was hit, run the rainbow logic
+        if (isRainbowActive)
+        {
+            ApplyRainbowColor();
+        }
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             Reset();
         }
-
     }
+
+    void ApplyRainbowColor()
+    {
+        float hue = (Time.time * colorChangeSpeed) % 1f;
+        Color rainbowColor = Color.HSVToRGB(hue, 1f, 1f);
+
+        ChangeColor(rainbowColor);
+    }
+
     void Reset()
     {
+        isRainbowActive = false; // Stop the rainbow
         ChangeColor(originalColor);
     }
+
     void ChangeColor(Color colorToSet)
     {
         if (rend != null)
@@ -33,33 +51,24 @@ public class ColliderTest : MonoBehaviour
     }
     #endregion
 
+    // Triggered when something enters the zone
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     isRainbowActive = true;
+    // }
+
+    // void OnTriggerExit(Collider other)
+    // {
+    //     Reset();
+    // }
+
     void OnCollisionEnter(Collision collision)
     {
-
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
-
+        isRainbowActive = true;
     }
 
     void OnCollisionExit(Collision collision)
     {
-
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-
-    }
-
-    void OnTriggerStay(Collider other)
-    {
-
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-
+        Reset();
     }
 }
